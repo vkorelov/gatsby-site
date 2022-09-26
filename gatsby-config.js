2f8aa6b4bd0653d3c92efa0285bb4304d797f0ba
vkorelov <elgreeko@mail.ru>
2022-09-26 14:02:40 +0300

    Gatsby tut 3

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,5 +3,8 @@ module.exports = {
     title: `gatsby-site`,
     siteUrl: `https://www.yourdomain.tld`,
   },
-  plugins: [],
+  plugins: [
+    "gatsby-plugin-image",
+    "gatsby-plugin-sharp",
+  ],
 }
